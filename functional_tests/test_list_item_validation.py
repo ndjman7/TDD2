3b9daab73ef14af953d6fa1c735e88e5ac708f44
pando <ndjman7@gmail.com>
2018-03-01 17:54:20 +0900

    Moved Fts into their own individual files

---
+++ functional_tests/test_list_item_validation.py
@@ -0,0 +1,24 @@
+from unittest import skip
+from selenium.webdriver.common.keys import Keys
+from .base import FunctionalTest
+
+
+class ItemValidationTest(FunctionalTest):
+
+    @skip
+    def test_cannot_add_empty_list_items(self):
+        # Pnado goes to the home page and accdientally tries to submit
+        # an empty list item. She hits Enter on the empty input box
+
+        # The home page refreshes, and there is an error message saying
+        # that list items cannot be blank
+
+        # He tries again with some text for the item, which new works
+
+        # Perversely, she now decides to submit a second blank list item
+
+        # He receives a similar warning on the list page
+
+        # And she can correct it by filling some text in
+        self.fail('write me!')
+
